ba479a39a2c8732813660a7ec495abc99cf635e5
ChuanPien <thomastw.lee@gmail.com>
2023-07-27 11:50:29 +0800

    modified:   app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+# 載入 LINE Message API 及 flask 相關函式庫
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from flask import Flask, request, abort
 from logs.log import log
 import json
 
-# 載入 LINE Message API 相關函式庫
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+#讀取設定檔
 with open('config.json', 'r', encoding='utf8')as J:
     j = json.load(J)
 
@@ -13,6 +14,7 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(j['token'])
 handler = WebhookHandler(j['secret'])
 
+#基礎碼
 @app.route("/", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']      # 加入回傳的 headers
@@ -23,12 +25,13 @@ def callback():
         abort(400)
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)         #如果訊息是文字
+#接收Line客戶端訊息
+@handler.add(MessageEvent, message=TextMessage)                                     #如果訊息是文字
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='test'))  #回傳'test'
-    id = event.source.user_id        #抓取使用者id
-    profile = line_bot_api.get_profile(id)        #抓取使用者資料
-    log(id, profile.display_name, event.message.text)        #紀錄log中
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='test'))      #回傳'test'
+    id = event.source.user_id                                                       #抓取使用者id
+    profile = line_bot_api.get_profile(id)                                          #抓取使用者資料
+    log(id, profile.display_name, event.message.text)                               #紀錄log中
 
 if __name__ == "__main__":
     app.run()
