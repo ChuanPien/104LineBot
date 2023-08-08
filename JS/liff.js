1401a483781ce30ec61354959f2c50251a61699d
ChuanPien <thomastw.lee@gmail.com>
2023-08-08 15:24:35 +0800

    modified:   JS/liff.js 	modified:   index.html 	modified:   style.css

--- JS/liff.js
+++ JS/liff.js
@@ -1,24 +1,24 @@
-function pushMsg(continent, country, town, jobs, job) {
-    if (continent == '' || country == '' || town == '' || jobs == '' || job == '') {  //資料檢查
-        alert('每個項目都必須輸入！');
-        return;
-    }
-    var msg = "###";  //回傳訊息字串
-    msg += continent + "/";
-    msg += country + "/";
-    msg += town + "/";
-    msg += jobs + "/";
-    msg += job;
-    liff.sendMessages([  //推播訊息
-        {
-            type: 'text',
-            text: msg
-        }
-    ])
-        .then(() => {
-            liff.closeWindow();  //關閉視窗
-        });
-}
+// function pushMsg(continent, country, town, jobs, job) {
+//     if (continent == '' || country == '' || town == '' || jobs == '' || job == '') {  //資料檢查
+//         alert('每個項目都必須輸入！');
+//         return;
+//     }
+//     var msg = "###";  //回傳訊息字串
+//     msg += continent + "/";
+//     msg += country + "/";
+//     msg += town + "/";
+//     msg += jobs + "/";
+//     msg += job;
+//     liff.sendMessages([  //推播訊息
+//         {
+//             type: 'text',
+//             text: msg
+//         }
+//     ])
+//         .then(() => {
+//             liff.closeWindow();  //關閉視窗
+//         });
+// }
 
 var liffID = '2000268560-PDBzXMGm'
 liff.init({
@@ -29,7 +29,7 @@ liff.init({
     // var context = liff.getContext();
     console.log(userid);
 
-    $('#btn').click(function () {  //按下確定鈕
-        pushMsg($("#continent-list :selected").text(), $("#country-list :selected").text(), $("#town-list :selected").text(), $("#job-main-list :selected").text(), $("#job-list :selected").text());
-    });
+    // $('#btn').click(function () {  //按下確定鈕
+    //     pushMsg($("#continent-list :selected").text(), $("#country-list :selected").text(), $("#town-list :selected").text(), $("#job-main-list :selected").text(), $("#job-list :selected").text());
+    // });
 });
