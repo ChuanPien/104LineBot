46275aa4d65085ec063ec42ea70c3e9d97364d0d
ChuanPien <thomastw.lee@gmail.com>
2023-08-08 15:52:44 +0800

    modified:   JS/liff.js 	modified:   style.css

--- JS/liff.js
+++ JS/liff.js
@@ -1,34 +1,44 @@
-// function pushMsg(continent, country, town, jobs, job) {
-//     if (continent == '' || country == '' || town == '' || jobs == '' || job == '') {  //資料檢查
-//         alert('每個項目都必須輸入！');
-//         return;
-//     }
-//     var msg = "###";  //回傳訊息字串
-//     msg += continent + "/";
-//     msg += country + "/";
-//     msg += town + "/";
-//     msg += jobs + "/";
-//     msg += job;
-//     liff.sendMessages([  //推播訊息
-//         {
-//             type: 'text',
-//             text: msg
-//         }
-//     ])
-//         .then(() => {
-//             liff.closeWindow();  //關閉視窗
-//         });
-// }
+function pushMsg(continent, country, town, jobs, job) {
+    // if (continent == '' || country == '' || town == '' || jobs == '' || job == '') {  //資料檢查
+    //     alert('每個項目都必須輸入！');
+    //     return;
+    // }
+    var msg = "###";  //回傳訊息字串
+    msg += continent + "/";
+    msg += country + "/";
+    msg += town + "/";
+    msg += jobs + "/";
+    msg += job;
+
+    liff.sendMessages([
+        {
+            type: 'text',
+            text: msg
+        }
+    ])
+        .then(() => {
+            console.log('message sent');
+        })
+        .catch((err) => {
+            console.log('error', err);
+        });
+}
+
 var liffID = '2000268560-PDBzXMGm'
 $(document).ready(function () {
     liff.init({
         liffId: liffID
-    }).then(function (data) {  //初始化LIFF
-        console.log("LIFF Start")
-        initializeApp(data);
+    }).then(function () {  //初始化LIFF
+        var isInClient = liff.isInClient();
+        if (!isInClient) {
+            alert("請用line開啟此網頁呦~");
+        } else {
+            var context = liff.getContext();
+            console.log(context);
 
-        // $('#btn').click(function () {  //按下確定鈕
-        //     pushMsg($("#continent-list :selected").text(), $("#country-list :selected").text(), $("#town-list :selected").text(), $("#job-main-list :selected").text(), $("#job-list :selected").text());
-        // });
+            $('#btn').click(function () {  //按下確定鈕
+                pushMsg($("#continent-list :selected").text(), $("#country-list :selected").text(), $("#town-list :selected").text(), $("#job-main-list :selected").text(), $("#job-list :selected").text());
+            });
+        }
     });
 });
